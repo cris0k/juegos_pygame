da559157a3100a8698b4ba8a031614d40c7a499a
cristina <criskukhar@hotmail.com>
2022-02-17 23:50:38 +0100

    Bola extra + definicion de padre

--- game.py
+++ game.py
@@ -1,57 +1,76 @@
+from curses import BUTTON1_RELEASED
 import pygame as pg
+import random 
 pg.init()
 
-class Ball:
-    def __init__(self, x, y, color = (255,255,255), radio= 10): #Atributos por defecto. Si mas tarde no lo definimos,estos seran los atributos
+class Bola:
+    def __init__(self, padre, x, y, color = (255, 255, 255), radio = 10):
         self.x = x
         self.y = y
         self.color = color
         self.radio = radio
-        self.vx = 1 #velocidad x hacia donde va a ir
-        self.vy = 1 #velocidad y hacia donde va a ir
-    
-    def move(self, right_limit, inferior_limit):
-        self.x += self.vx
+        self.vx = 1
+        self.vy = 1
+        self.padre = padre
+
+
+    def mover(self):
+        self.x += self.vx 
         self.y += self.vy
 
-
-        if self.x <= self.radio or self.x >=right_limit - self.radio:
+        if self.x <= self.radio or self.x >= self.padre.get_width() - self.radio:
             self.vx *= -1
-            #if self.radio >= self.x >= right_limit - self.radio:  esto es lo mismo que lo de arriba
 
-        if self.y <= self.radio or self.y >= inferior_limit - self.radio:
+        #if self.radio >= self.x >= limDer - self.radio:
+
+        if self.y <= self.radio or self.y >= self.padre.get_height() - self.radio:
             self.vy *= -1
 
-    def draw (self, surface): # Paint the circle
-        pg.draw.circle(surface, self.color,(self.x, self.y), self.radio) 
-
+    def dibujar(self):
+        pg.draw.circle(self.padre, self.color, (self.x, self.y), self.radio)
 
 class Game:
-    def __init__(self, width=600, height=800):
-        self.screen = pg.display.set_mode((width, height))
-        self.ball = Ball(width // 2, height // 2, (255, 255,0)) # Division entera de la posicion(sin decimales) + cambio de color de bola
+    def __init__(self, ancho=600, alto=800):
+        self.pantalla = pg.display.set_mode((ancho, alto))
+        self.bola = Bola(self.pantalla, ancho // 2, alto // 2, (255, 255, 0))
+        self.bolas = []
+        """
+        for i... random de numero de bolas:
+            self.bolas.append(nueva_bola)
+            diferentes colores
+            diferentes tamaños
+            diferentes velocidades
+        """
 
-    def main_loop(self):
+        self.bola1 = Bola(self.pantalla, 350, 250, radio=60)
+        self.bola1.vx = -1
+        self.bola1.vy = -1
+
+    def bucle_ppal(self):
         game_over = False
 
         while not game_over:
-            events = pg.event.get()
 
-            for event in events:
-                if event.type == pg.QUIT:
+            eventos = pg.event.get()
+            for evento in eventos:
+                if evento.type == pg.QUIT:
                     game_over = True
-            
-            self.ball.move(self.screen.get_width(), self.screen.get_height()) #get width y get height esta predefinido por pygame
-            self.screen.fill((255, 0, 0))
-            self.ball.draw(self.screen)
-            
 
+            #mover todas las bolas
+            self.bola.mover()
+            self.bola1.mover()
+            self.pantalla.fill((255, 0, 0))    
+            #dibujar todas las bolas
+            self.bola.dibujar()
+            self.bola1.dibujar()
+            
             pg.display.flip()
 
-if __name__== '__main__':
+    
+if __name__ == '__main__':
     pg.init()
 
-    game = Game() # crea el juego
-    game.main_loop() # hace funcionar el juego
+    game = Game()
+    game.bucle_ppal()
 
     pg.quit()
